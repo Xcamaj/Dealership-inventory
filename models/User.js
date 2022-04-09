472df1531044cb32fd931bcd0a75df29be94941f
PrashantP <prashantpatel622@gmail.com>
2022-04-09 10:01:47 -0400

    all Seeds working

--- models/User.js
+++ models/User.js
@@ -3,9 +3,9 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-      }
+    // checkPassword(loginPw) {
+    //     return bcrypt.compareSync(loginPw, this.password);
+    //   }
 }
 
 User.init(
@@ -33,22 +33,23 @@ User.init(
           allowNull: false,
           }
     },
-    {
-        hooks:{
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 20);
-                return newUserData;
-              },
-              async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 20);
-                return updatedUserData;
-              }
-        },
+    // {
+    //     hooks:{
+    //         async beforeCreate(newUserData) {
+    //             newUserData.password = await bcrypt.hash(newUserData.password, 20);
+    //             return newUserData;
+    //           },
+    //           async beforeUpdate(updatedUserData) {
+    //             updatedUserData.password = await bcrypt.hash(updatedUserData.password, 20);
+    //             return updatedUserData;
+    //           }
+    //     },
+      {
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelname: 'user'
+        modelname: 'User'
     }
 );
 
