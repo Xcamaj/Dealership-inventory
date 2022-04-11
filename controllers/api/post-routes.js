b45d299bb196e71735766783655efa86998898d1
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-04-11 13:24:38 -0400

    Routing done, need to test

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,22 +1,245 @@
 const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const {NewCars, UsedCars} = require('../../models')
+const { NewCars, UsedCars, User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res)=> {
-    NewCars.findall({
-        attributes: [
-            'id',
-            'make',
-            'model',
-            'price',
-            'type'
-        ],
-    })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+// get all users (new)
+router.get('/', (req, res) => {
+  console.log('======================');
+  NewCars.findAll({
+    attributes: [
+        'id',
+        'make',
+        'mode',
+        'price',
+        'type'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-module.exports = router
+router.get('/:id', (req, res) => {
+  NewCars.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+        'id',
+        'make',
+        'model',
+        'price',
+        'type'
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  NewCars.create({
+    make: req.body.make,
+    model: req.body.model,
+    price: req.body.price,
+    type: req.body.type,
+    user_id: req.session.user_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+  NewCars.update(
+    {
+        make: req.body.make,
+        model: req.body.model,
+        price: req.body.price,
+        type: req.body.type
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+  console.log('id', req.params.id);
+  NewCars.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//OLD CARS
+router.get('/', (req, res) => {
+    console.log('======================');
+    UsedCars.findAll({
+      attributes: [
+          'id',
+          'make',
+          'mode',
+          'price',
+          'type'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.get('/:id', (req, res) => {
+    UsedCars.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+          'id',
+          'make',
+          'model',
+          'price',
+          'type'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.post('/', withAuth, (req, res) => {
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    UsedCars.create({
+      make: req.body.make,
+      model: req.body.model,
+      price: req.body.price,
+      type: req.body.type,
+      user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.put('/:id', withAuth, (req, res) => {
+    UsedCars.update(
+      {
+          make: req.body.make,
+          model: req.body.model,
+          price: req.body.price,
+          type: req.body.type
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.delete('/:id', withAuth, (req, res) => {
+    console.log('id', req.params.id);
+    UsedCars.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
