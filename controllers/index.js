a6b116e1c3d2cbb54d99647069d046f621b68065
Franklyn Tavarez <kurbby.450@gmail.com>
2022-04-13 00:56:06 -0700

    handlebars part1

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
+const handlebarsRoutes = require('./handlebars-routs')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/handlebars', handlebarsRoutes)
 
 router.use((req, res) => {
     res.status(404).end();
