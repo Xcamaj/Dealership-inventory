70cb36306609b356c7d24fb15925f247f89d06b0
PrashantP <prashantpatel622@gmail.com>
2022-04-17 10:34:10 -0400

    login, css updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -145,16 +145,19 @@ router.get('/usedcars', (req, res) => {
 //   });
 
 router.get('/login', (req, res) => {
-  // if (req.session.loggedIn) {
-  //   res.redirect('/');
-  //   return;
-  // }
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
   res.render('login');
 });
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+ 
+  res.render('homepage' , {
+    loggedIn: req.session.loggedIn
+  });
 });
 
 // router.get('/newcars', (req, res) => {
